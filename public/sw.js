1daa0c3f2986df568431412de901ceb53fef8f23
m <e@e.jp>
2026-04-15 16:26:49 +0900

    オフライン化(PWA)

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+var CACHE_NAME = 'paint-app-v1';
+var ASSETS = [
+	'/',
+	'/index.html',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(ASSETS);
+			})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
+});
